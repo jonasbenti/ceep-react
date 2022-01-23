943a4e366e1589ef6ca89f2c073b21fd6f30ca9a
Jonas <jonasbenti@hotmail.com>
2022-01-23 10:58:18 -0300

    Etapa1: criação das notas concluidas

--- src/App.js
+++ src/App.js
@@ -4,11 +4,33 @@ import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
 class App extends Component {
+
+  constructor(){
+    super();
+    // this.notas = [];
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(id, titulo, texto){
+    // console.log('Nova nota criada: ' + titulo + ' ' + texto);
+    const novaNota = {id, titulo, texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    // this.notas.push(novaNota);
+    this.setState(novoEstado);
+    // this.render();
+  }  
+
   render() {
+    // console.log("Entrou Render");
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,18 +2,60 @@ import React, { Component } from 'react';
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+
+    constructor(props) {
+        super(props);
+        this.titulo = "";
+        this.texto = "";
+        this.id = 0;
+    }
+
+    _handleMudancaTitulo(evento){
+        evento.stopPropagation();
+        this.titulo = evento.target.value;
+    }
+
+    _handleMudancaTexto(evento){
+        evento.stopPropagation();
+        this.texto = evento.target.value;
+    }
+
+    _criarNota(evento){
+        evento.preventDefault();
+        evento.stopPropagation();
+
+        if(this.titulo === '' || this.texto === '') {
+            alert("Prencha os campos para criar a nota")
+        } else {
+            this.id++;
+            // console.log(`id: ${this.id}, tit: ${this.titulo}, text: ${this.texto}`)
+            this.props.criarNota(this.id, this.titulo, this.texto);
+            this.titulo = "";
+            this.texto = "";
+            document.getElementById("titulo").value = "";
+            document.getElementById("texto").value = "";
+        }
+        
+    }
+
     render() {
         return (
-            <form className="form-cadastro ">
+            <form className="form-cadastro"
+                onSubmit={this._criarNota.bind(this)}
+            >
                 <input 
+                    id="titulo"
                     type="text"
                     placeholder='Título'
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaTitulo.bind(this)}
                 />
                 <textarea 
+                    id="texto"
                     rows={15}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
                     Criar Nota
